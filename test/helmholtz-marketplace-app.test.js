882d11782e1f9dab2ad39a7c62605ae2b4228cd5
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2020-06-09 15:13:27 +0200

    add correct tab spacing to the files (part 2 - leftover)
    I forgot to do the indentation of these following files:
        - helmholtz-marketplace-app.js
        - helmholtz-marketplace-app.test.js
    
    Also, the `prettier` is configured to use 4 spacing as
    the default indentation.
    
    Pull-request: #6

--- test/helmholtz-marketplace-app.test.js
+++ test/helmholtz-marketplace-app.test.js
@@ -3,20 +3,20 @@ import { html, fixture, expect } from '@open-wc/testing';
 import '../src/helmholtz-marketplace-app.js';
 
 describe('Helmholtz Marketplace App Shell', () => {
-  let element;
-  beforeEach(async () => {
-    element = await fixture(html`
-      <helmholtz-marketplace-app></helmholtz-marketplace-app>
-    `);
-  });
-  it('renders a header', () => {
-    const header = element.shadowRoot.querySelector('mwc-top-app-bar-fixed');
-    expect(header).to.exist;
-    expect(header.classList.contains('header')).be.true;
-  });
-  it('renders a body', async () => {
-    const body = element.shadowRoot.querySelector('.body');
-    expect(body).to.exist;
-    expect(body.classList.contains('body')).be.true;
-  });
+    let element;
+    beforeEach(async () => {
+        element = await fixture(html`
+            <helmholtz-marketplace-app></helmholtz-marketplace-app>
+        `);
+    });
+    it('renders a header', () => {
+        const header = element.shadowRoot.querySelector('mwc-top-app-bar-fixed');
+        expect(header).to.exist;
+        expect(header.classList.contains('header')).be.true;
+    });
+    it('renders a body', async () => {
+        const body = element.shadowRoot.querySelector('.body');
+        expect(body).to.exist;
+        expect(body.classList.contains('body')).be.true;
+    });
 });
